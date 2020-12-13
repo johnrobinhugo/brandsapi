7ae69974e21773f488c844886fc86b002ca2c5f2
Robin Stenkilson <robinstenkilson@Robins-MacBook-Air.local>
2020-12-13 16:59:46 +0100

    Added localhost:8080 as allowed origin

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const mongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
 const db = require('./config/db')
@@ -8,7 +9,13 @@ const app = express()
 
 const port = 8000
 
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.use(bodyParser.urlencoded({ extended: true}))
+app.use(cors(corsOptions))
 
 mongoClient.connect(db.url, (err, client) => {
   if (err) return console.log(err)
