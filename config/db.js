4ed9b4cd12fee94ebde4d9a5816246e2c35e206d
Robin Stenkilson <robinstenkilson@Robins-MacBook-Air.local>
2020-12-13 16:00:03 +0100

    Support for .env file

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@robdev.hm81v.mongodb.net/brandsapi?retryWrites=true&w=majority`
+}
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const mongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
